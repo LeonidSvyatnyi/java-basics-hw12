ed7ac5a2b3624adc3718e16717968378e908d30a
leonid.svyatnyi <svyatnyi.leonid@gmail.com>
2022-06-01 21:08:40 +0300

    Task Done

--- src/StreamTask.java
+++ src/StreamTask.java
@@ -1,12 +1,17 @@
+import java.util.Arrays;
+
 public class StreamTask {
 
     public static int getSumSquareEven(int[] numbers) {
-        // TODO replace return with your code
-        return 0;
+        return Arrays.stream(numbers)
+            .filter(a -> a % 2 == 0)
+            .reduce(0, (a, b) -> a + b * b);
     }
 
     public static int[] getSortedOddsArray(int[] numbers) {
-        // TODO replace return with your code
-        return new int[]{};
+        return Arrays.stream(numbers)
+            .filter(a -> Math.abs(a) % 2 == 1)
+            .sorted()
+            .toArray();
     }
 }
